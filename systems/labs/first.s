a72c961585a115e5f8ae113723b92a6325c94c5b
Kirill Bezuglyi <fidelicura@gmail.com>
2024-09-03 12:00:22 +0300

    finished first

--- systems/labs/first.s
+++ systems/labs/first.s
@@ -1,26 +1,33 @@
 # Made by Kirill Bezuglyi in GNU ASM with AT&T syntax for x86-64.
 #
-# Task:
+# Tasks:
 #
 #      9 * 11 * 2 + 4 * 11 * 21
-# x = ────────────────────────── = -51.0
+# x = ────────────────────────── = -51
 #         2^2 - 2 * (11 + 2)
+#
+#          -51 * 21 - 2^2
+# y = ────────────────────────── = 7
+#      16 * (11 - 14) + 2 * -51
 
 .section .data # global data goes here
 
-msg: .ascii "result is " # output text for a user
+msg: .asciz "result is -51 and 7\n" # output text for a user
 msg_len = . - msg # length of output text
-buffer: .space 6 # store a ascii result value with zero byte
 
 .section .text # code goes here
 
 .global _start # set an entry label
 _start:
+        # PREPARATIONS
+
         movq $11, %rdi # a = 11 in %rdi
         movq $2, %rsi # b = 2 in %rsi
         movq $21, %rdx # c = 21 in %rdx
         movq $-51, %rcx # x = -51 in %rcx
         movq $7, %r8 # y = 7 in %r8
+
+        # FIRST ITERATION
 
         xorq %r10, %r10 # first temp storage
         xorq %r11, %r11 # second temp storage
@@ -30,33 +37,40 @@ _start:
         imulq $4, %rdi, %r11 # 4 * 11 = 44, stored in %r11
         imulq %rdx, %r11 # 44 (from %r11) * 21 = 924, stored in %r11
         addq %r11, %r10 # 198 + 924 = 1122, stored in %r10
-        push %r10 # push top expression result to stack
+        pushq %r10 # save top expression result via pushing into stack
         
-        imulq %rsi, %rsi, %r10 # 2 (from %rsi) * 2 (from %rsi) = 4, stored in %r10
-        movq %rdi, %r11 # move 11 from %rdi into %r11 temp storage for addition
+        movq %rsi, %r10 # move 2 from %rsi into $r10 temp storage for further multiplication
+        imulq %r10, %r10 # 2 (from %r10) * 2 (from %r10) = 4, stored in %r10
+        movq %rdi, %r11 # move 11 from %rdi into %r11 temp storage for further addition
         addq $2, %r11 # 11 (from %rdi) + 2 = 13, stored in %r11
         imulq $2, %r11 # 2 * 13 = 26, stored in %r11
-        subq %r10, %r11 # 4 (from $r10) - 26 = -22, stored in %r11
+        subq %r11, %r10 # 4 (from %r10) - 26 (from %r11) = -22, stored in %r10
+        popq %rax # restore result of top expression into %rax
+        xorq %rdx, %rdx # clear the quotient register
+        idivq %r10 # 1122 (from %rax) / -22 (from %r10) = -51.0, stored in %rdx
 
-        pop %rdx # restore result of top expression into %rdx
-        imulq %r11 # 1122 (from %rdx) / -22 (from %r11) = -51.0, stored in %rax
+        # SECOND ITERATION
 
-        movq %rax, %rdi # move the result into %rdi to preserve %rax
-        lea buffer(%rip), %rsi # load buffer address into %rsi
-        movq $0, %rcx # clear %rcx to count number of digits
-        jmp convert # convert result value to ascii
+        xorq %r10, %r10 # first temp storage
+        xorq %r11, %r11 # second temp storage
+        movq $21, %rdx # restore 21 in %rdx
 
-convert:
-        xorq %rdx, %rdx # clear %rdx to avoid integer division overflow
-        movq $10, %rbx # set divisor to 10
-        idivq %rbx # divide result from %rdi by 10 from %rbx
-        addq $'0', %rdx # convert to ascii by offset from adding
-        movb %dl, (%rsi) # store converted digit in buffer
-        incq %rsi # move the pointer by offset of 1 to the next position
-        incq %rcx # increment the digit counter
-        testq %rax, %rax # check if quotient is zero
-        jnz convert # if not zero, repeat the convertation step
-        jmp print_message # else print the value from the buffer with the message
+        imulq %rcx, %rdx # -51 (from %rcx) * 21 (from %rdx) = -1071, stored in %rdx
+        imulq %rsi, %rsi # 2 (from %rsi) * 2 (from %rsi) = 4, stored in %r10
+        subq %rsi, %rdx # -1071 (from %rsi) - 4 (from %rdx) = -1075, stored in %rdx
+        pushq %rdx # save top expression result via pushing into stack
+
+        subq $14, %rdi # 11 (from %rdi) - 14 = -3, stored in %rdi
+        imulq $16, %rdi # 16 * (-3) (from %rdi) = -48, stored in %rdi 
+        imulq $2, %rcx # 2 * (-51) (from %rcx) = -102, stored in %rcx
+        addq %rcx, %rdi # -48 (from %rcx) + (-102) (from %rdi) = -150, stored in %rdi
+        popq %rax # restore result of top expression into %rax
+        cqto # sign extend %rax into %rdx
+        idivq %rdi # -1075 (from %rax) / (-150) (from %rdi) = 7, stored in %rax
+        
+        # END OF ITERATIONS
+
+        jmp print_message # print the predefined message for the user
 
 print_message:
         movq $1, %rax # number 1 is a syscall for `write`
@@ -64,17 +78,7 @@ print_message:
         lea msg(%rip), %rsi # address of the message
         movq $msg_len, %rdx # message length
         syscall # call a syscall
-        jmp print_digit # print the digits from a value
-
-print_digit:
-        movq $1, %rax # number 1 is a syscall for `write`
-        movq $1, %rdi # number 1 is a fd for `stdout`
-        movb (%rsi), %al # load the byte of value to print
-        syscall # call a syscall
-        decq %rsi # move to the previous digit
-        decq %rdx # decrement digit count
-        jnz print_digit # if there are more digits to print, then repeat
-        jmp exit # else exit at the end of the program
+        jmp exit # exit at the end of the program
 
 exit:
         movq $60, %rax # number 60 is a syscall for `exit`
