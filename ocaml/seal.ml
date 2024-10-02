dfd86adfd02ccb7c5a0208d9515b0a1ecbf7eead
Kirill Bezuglyi <fidelicura@gmail.com>
2024-10-02 12:44:18 +0300

    small correction on sealing in ocaml

--- ocaml/seal.ml
+++ ocaml/seal.ml
@@ -4,7 +4,7 @@ module type StackSig = sig
   val empty : 'a t
 end
 
-(* EXAMPLE №1: SAVED CONSISTENCY WITH THE INTERFACE, BUT SEALED AT DECLARATION *)
+(* EXAMPLE №1: SAVED CONSISTENCY WITH THE INTERFACE (SIGNATURE), BUT SEALED AT DECLARATION *)
 module Stack : StackSig = struct
   type 'a t = 'a List.t
 
@@ -14,7 +14,7 @@ end
 let example_ok : int Stack.t = Stack.empty
 let example_err : int Stack.t = [] (* error: expected `int Stack.t`, got `int List.t` *)
 
-(* EXAMPLE №2: SAVED CONSISTENCY WITH THE INTERFACE, BUT WAS NOT SEALED*)
+(* EXAMPLE №2: SAVED CONSISTENCY WITH THE INTERFACE (SIGNATURE), BUT WAS NOT SEALED *)
 module StackImpl = struct
   type 'a t = 'a List.t
 
@@ -24,9 +24,10 @@ end
 module Stack : StackSig = StackImpl
 
 let example_ok : int Stack.t = Stack.empty
-let example_err : int Stack.t = [] (* no error: it was not sealed at declaration*)
+let example_err : int StackImpl.t = [] (* no error: it was not sealed at declaration *)
 
 (** EXPLANATION: in a second example, when we assign `StackImpl` module to newly created module `Stack`,
     OCaml had only just checked the consistency with interface (`: StackSig`) of `StackImpl`, but haven't
     sealed any declarations inside `StackImpl` because we _assigned_ it to the newly created module `Stack`,
-    that now represents implementation of a `StackSig` signature we just created. *)
+    that now represents implementation of a `StackSig` signature we just created. Now, `Stack` has sealed
+    interface definitions, but `StackImpl` has not and we can use it as if we got no abstraction type of `t`. *)
