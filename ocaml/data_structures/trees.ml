39a95e376dd0ca4a2339bbf62fcf610f90c68acd
Kirill Bezuglyi <fidelicura@gmail.com>
2024-10-17 13:07:53 +0300

    finish redblack tree

--- ocaml/data_structures/trees.ml
+++ ocaml/data_structures/trees.ml
@@ -15,15 +15,52 @@ module BinaryTree: Tree = struct
 
   let rec insert v = function
     | Leaf -> Node { l = Leaf; v = v; r = Leaf }
-    | Node { l = l'; v = v'; r = r' } as curr ->
+    | Node { l = l'; v = v'; r = r' } as self ->
       if v < v' then Node { l = insert v l'; v = v'; r = r' }
       else if v > v' then Node { l = l'; v = v'; r = insert v r' }
-      else curr
+      else self
 
   let rec mem v = function
     | Leaf -> None
-    | Node { l = l'; v = v'; r = r' } as curr ->
+    | Node { l = l'; v = v'; r = r' } as self ->
       if v < v' then mem v l'
       else if v > v' then mem v r'
-      else Some curr
+      else Some self
+end
+
+module RedBlackTree: Tree = struct
+  type 'a t =
+    | Leaf
+    | Node of color * 'a * 'a t * 'a t
+  and color =
+    | Red
+    | Black
+
+  let balance = function
+    | Node (Black, z, Node (Red, y, Node (Red, x, a, b), c), d)
+    | Node (Black, z, Node (Red, x, a, Node (Red, y, b, c)), d)
+    | Node (Black, x, a, Node (Red, z, Node (Red, y, b, c), d))
+    | Node (Black, x, a, Node (Red, y, b, Node (Red, z, c, d)))
+      -> Node (Red, y, Node (Black, x, a, b), Node (Black, z, c, d))
+    | Node (a, b, c, d) as self -> self
+    | Leaf -> failwith "unreachable"
+
+  let rec insert_aux v = function
+    | Leaf -> Node (Red, v, Leaf, Leaf)
+    | Node (_, v', l, r) as self ->
+      if v > v' then balance (insert_aux v r)
+      else if v < v' then balance (insert_aux v l)
+      else self
+
+  let insert v t =
+    match insert_aux v t with
+    | Leaf -> failwith "unreachable"
+    | Node (_, v', l, r) -> Node (Black, v', l, r)
+
+  let rec mem v = function
+    | Leaf -> None
+    | Node (_, v', l, r) as self ->
+      if v < v' then mem v r
+      else if v > v' then mem v l
+      else Some self
 end
