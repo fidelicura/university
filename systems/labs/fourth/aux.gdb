18d30d4c0a435fad50106fada2b5be5d086a4e7d
Kirill Bezuglyi <fidelicura@gmail.com>
2024-11-20 16:58:41 +0300

    imporvement in fourth lab

---
+++ systems/labs/fourth/aux.gdb
@@ -0,0 +1,12 @@
+define print_info
+	printf "array #1: first value = %d\n", (long long)first_first_value
+	printf "array #1: first index = %d\n", (long long)first_first_index
+	printf "array #1: second value = %d\n", (long long)first_second_value
+	printf "array #1: second index = %d\n", (long long)first_second_index
+
+	printf "array #2: first value = %d\n", (long long)second_first_value
+	printf "array #2: first index = %d\n", (long long)second_first_index
+	printf "array #2: second value = %d\n", (long long)second_second_value
+	printf "array #2: second index = %d\n", (long long)second_second_index
+end
+
--- systems/labs/fourth/main.s
+++ systems/labs/fourth/main.s
@@ -14,8 +14,11 @@
 # 
 # Expectations:
 #
-# (first_value, first_index) = (-10, 0),
-# (second_value, second_index) = (-7, 8).
+# (first_first_value, first_first_index) = (-10, 0),
+# (first_second_value, first_second_index) = (-7, 8).
+#
+# (second_first_value, second_first_index) = (-11, 5),
+# (second_second_value, second_second_index) = (-9, 1).
 
         # platform-specific for x86-64
         .set LLONG_MAX, 9223372036854775807
@@ -25,30 +28,46 @@
 
 .section .rodata
 
-        array_data: .8byte -10, 40, -2, 6, 80, -1, 3, 12, -7, 5
-        array_info:
-                array_size = array_info - array_data
-                array_length = array_size / LLONG_SIZE - 1 # amortized for zero-based index
+        first_array_data: .8byte -10, 40, -2, 6, 80, -1, 3, 12, -7, 5
+        first_array_info:
+                first_array_size = first_array_info - first_array_data
+                first_array_length = first_array_size / LLONG_SIZE - 1 # amortized for zero-based index
+
+        second_array_data: .8byte 13, -9, 100, 31, -3, -11, 76
+        second_array_info:
+                second_array_size = second_array_info - second_array_data
+                second_array_length = second_array_size / LLONG_SIZE - 1 # amortized for zero-based index
 
 .section .data
 
-        first_value: .8byte LLONG_MAX
-        second_value: .8byte LLONG_MAX
+        first_first_value: .8byte LLONG_MAX
+        first_second_value: .8byte LLONG_MAX
+        second_first_value: .8byte LLONG_MAX
+        second_second_value: .8byte LLONG_MAX
 
-        first_index: .8byte DEFAULT_INDEX
-        second_index: .8byte DEFAULT_INDEX
+        first_first_index: .8byte DEFAULT_INDEX
+        first_second_index: .8byte DEFAULT_INDEX
+        second_first_index: .8byte DEFAULT_INDEX
+        second_second_index: .8byte DEFAULT_INDEX
 
 .section .text
 
         .global _start
 
 _start:
-        movq   $array_data, %rdi    # store a pointer to an array
-        movq   $array_length, %rcx  # load an array length
-        movq   $first_value, %rsi   # copy initial value of first value
-        movq   $first_index, %rdx   # copy initial value of first index
-        movq   $second_value, %r8   # copy initial value of second value
-        movq   $second_index, %r9   # copy initial value of second index
+        movq   $first_array_data, %rdi     # store a pointer to an array
+        movq   $first_array_length, %rcx   # load an array length
+        movq   $first_first_value, %rsi    # copy initial value of first value
+        movq   $first_first_index, %rdx    # copy initial value of first index
+        movq   $first_second_value, %r8    # copy initial value of second value
+        movq   $first_second_index, %r9    # copy initial value of second index
+        callq  main
+        movq   $second_array_data, %rdi    # store a pointer to an array
+        movq   $second_array_length, %rcx  # load an array length
+        movq   $second_first_value, %rsi   # copy initial value of first value
+        movq   $second_first_index, %rdx   # copy initial value of first index
+        movq   $second_second_value, %r8   # copy initial value of second value
+        movq   $second_second_index, %r9   # copy initial value of second index
         callq  main
         jmp    exit
 
@@ -72,10 +91,11 @@ L_main_first:
         jmp   L_main_step
 
 L_main_second:
-        pushq  (%rdx)       # store `array[i]` value as a first value
-        popq   (%r8)        # load `array[i]` value as a first value
-        movq   %rcx, (%r9)  # and store `i` index as first index
-        jmp    L_main_step
+        movq  (%r8), %rbx   # save current second value in temp register
+        movq  (%r9), %r10   # save current second index in temp register
+        movq  %rax, (%r8)   # store current array value as new second value
+        movq  %rcx, (%r9)   # store current index as new second index
+        jmp   L_main_step
 
 L_main_step:
         decq  %rcx           # decrement loop counter value
